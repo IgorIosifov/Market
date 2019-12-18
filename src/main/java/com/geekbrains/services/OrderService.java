eacec345dc3f17eaa380863ac850bb66a22f6f93
IgorIosifov <igoriosifov@yahoo.com>
2019-12-18 20:54:57 +0300

    Lesson 1.Base project.

---
+++ src/main/java/com/geekbrains/services/OrderService.java
@@ -0,0 +1,20 @@
+package com.geekbrains.services;
+
+import com.geekbrains.entites.Order;
+import com.geekbrains.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public void setOrderRepository(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order save(Order order) {
+        return orderRepository.save(order);
+    }
+}
